0c5d05709a065acaba218ded8998cbc4ca3c4ab1
logancool <coollogan88@gmail.com>
2020-07-04 15:40:58 -0700

    add new font

--- src/Components/page/ResumePage/index.tsx
+++ src/Components/page/ResumePage/index.tsx
@@ -5,27 +5,31 @@ import './styles.module.scss';
 
 const ResumePage = () => (
     <PageContent>
-        <div styleName="columns">
-            <div styleName="headline">
-                <div styleName="headline-name">{data.headline.name}</div>
-                <span styleName="headline-description">{data.headline.role}</span>
-            </div>
-            <div styleName="experience">
-                {data.experience.map(job => (
-                    <div key={job.company}>
-                        <h3 styleName="section-title">
-                            <strong>{job.company}</strong>
-                            {`, ${job.location} \u2014 ${job.title}`}
-                        </h3>
-                        <h5 styleName="section-date">{`${job.date.start} \u2014 ${job.date.end}`}</h5>
+        <div styleName="resume">
+            <div styleName="columns">
+                <div styleName="headline">
+                    <div styleName="headline-name">{data.headline.name}</div>
+                    <span styleName="headline-description">
+                        <i>{data.headline.role}</i>
+                    </span>
+                </div>
+                <div styleName="experience">
+                    {data.experience.map(job => (
+                        <div key={job.company}>
+                            <h3 styleName="section-title">
+                                <strong>{job.company}</strong>
+                                {`, ${job.location} \u2014 ${job.title}`}
+                            </h3>
+                            <h5 styleName="section-date">{`${job.date.start} \u2014 ${job.date.end}`}</h5>
 
-                        <ul styleName="skills">
-                            {job.skills.map(skill => (
-                                <li key={skill}>{skill}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+                            <ul styleName="skills">
+                                {job.skills.map(skill => (
+                                    <li key={skill}>{skill}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     </PageContent>
--- src/components/page/HomePage/index.tsx
+++ src/components/page/HomePage/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PageContent from '../../layout/PageContent';
 import './styles.module.scss';
+import data from './data';
 
 const HomePage = () => (
     <PageContent>
         <div styleName="intro">
-            <p styleName="p">
-                Hi there 👋, Logan here. If you have ever met me, I consider myself as a very positive person who loves
-                music, travelling, and being outside. I moved to the bay area about 5 years ago from a beautiful farm up
-                in Canada...eh? I have always loved technology, and have been lucky enough/been told to have an eye for
-                detail. I love working with others, and I am always excited to hear other people’s ideas. Currently most
-                of my time is spent in the <code styleName="code">.front-end</code> bucket however on-call seems to
-                enlighten me of all of the other cool things happening on the web. Wishing you the best of days with no
-                connectivity issues. Thanks for taking the time to surf over my&nbsp;way.&nbsp;🌊
-            </p>
+            <p styleName="p intro-text">Hi there 👋, Logan here.</p>
+            <h3> Logan facts</h3>
+            <div styleName="facts">
+                {data.facts.map((fact, index) => (
+                    <li key={index} dangerouslySetInnerHTML={{ __html: fact }} styleName="fact" />
+                ))}
+            </div>
+            <p styleName="p">Thanks for taking the time to surf over my&nbsp;way.&nbsp;🌊</p>
         </div>
     </PageContent>
 );
