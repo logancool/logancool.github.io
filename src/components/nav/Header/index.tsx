c13e75d8e3f87c6e1c06be9e779b9ad6dbe72aa4
logancool <coollogan88@gmail.com>
2020-07-04 14:40:57 -0700

    fix active state on refresh

--- src/components/nav/Header/index.tsx
+++ src/components/nav/Header/index.tsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.module.scss';
 
 const Header = () => {
     const pages = ['me', 'work', 'resume', 'projects'];
+    const history = useHistory();
 
     const [activePage, setActive] = useState('me');
+
+    // if user refreshes page update active state
+    useEffect(() => {
+        if (history.location.pathname !== '/') {
+            setActive(history.location.pathname.replace('/', ''));
+        }
+    }, []);
 
     return (
         <header styleName="placeholder">
@@ -17,6 +25,7 @@ const Header = () => {
                                 styleName="link"
                                 onClick={() => {
                                     setActive(page);
+                                    history.push(`/${page}`);
                                 }}
                                 to={page === 'me' ? '/' : `/${page}`}
                             >
