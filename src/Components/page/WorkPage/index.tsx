02276ea52ef7b69f51ff6890fd27f316110a74b3
logancool <coollogan88@gmail.com>
2020-07-19 14:32:53 -0700

    add some font line-height standards and use grid for buttons

--- src/Components/page/WorkPage/index.tsx
+++ src/Components/page/WorkPage/index.tsx
@@ -4,20 +4,23 @@ import './styles.module.scss';
 
 const WorkPage = () => (
     <div styleName="work">
-        <div styleName="link">
+        <div styleName="full-width">
             <div styleName="sfix" href="https://www.stitchfix.com/">
                 <h2 styleName="h2"> Stitch Fix </h2>
                 <p styleName="p">
                     Since starting in 2017, I helped forefront the launch of Kids, Maternity, Plus, and the expansion to
                     the UK. My initial responsibilities included working directly with marketing & design building AB
                     tests for our Conversion team. Alongside another engineer and partnership of{' '}
-                    <a href="https://www.carbonfive.com/">Carbon 5</a>, we built a Node.js static site using Jekyll.
-                    Later, my team grew and began focusing on a customized static react app/rails server. During this
-                    time I led and broke down both internal and external facing projects using&nbsp;Jira.
+                    <a styleName="a" href="https://www.carbonfive.com/">
+                        Carbon 5
+                    </a>
+                    , we built a Node.js static site using Jekyll. Later, my team grew and began focusing on a
+                    customized static react app/rails server. During this time I led and broke down both internal and
+                    external facing projects using&nbsp;Jira.
                 </p>
             </div>
         </div>
-        <div styleName="link">
+        <div styleName="full-width">
             <div styleName="ft" href="https://www.flashtalking.com/">
                 <h2 styleName="h2">Flashtalking</h2>
                 <p styleName="p">
