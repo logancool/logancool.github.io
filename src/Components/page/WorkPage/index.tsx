caf71bc588fba66aaf100a4e400994c16dfe6935
logancool <coollogan88@gmail.com>
2020-07-19 17:31:50 -0700

    use grid and add icons for work

--- src/Components/page/WorkPage/index.tsx
+++ src/Components/page/WorkPage/index.tsx
@@ -4,33 +4,39 @@ import './styles.module.scss';
 
 const WorkPage = () => (
     <div styleName="work">
-        <div styleName="full-width">
-            <div styleName="sfix" href="https://www.stitchfix.com/">
-                <h2 styleName="h2"> Stitch Fix </h2>
-                <p styleName="p">
-                    Since starting in 2017, I helped forefront the launch of Kids, Maternity, Plus, and the expansion to
-                    the UK. My initial responsibilities included working directly with marketing & design building AB
-                    tests for our Conversion team. Alongside another engineer and partnership of{' '}
-                    <a styleName="a" href="https://www.carbonfive.com/">
-                        Carbon 5
-                    </a>
-                    , we built a Node.js static site using Jekyll. Later, my team grew and began focusing on a
-                    customized static react app/rails server. During this time I led and broke down both internal and
-                    external facing projects using&nbsp;Jira.
-                </p>
-            </div>
+        <div styleName="sfix" href="https://www.stitchfix.com/">
+            <h2 styleName="workplace">
+                <a href="https://www.stitchfix.com/"> Stitch Fix </a>
+            </h2>
+            <p styleName="p">
+                Since starting in 2017, I helped forefront the launch of Kids, Maternity, Plus, and the expansion to the
+                UK. My initial responsibilities included working directly with marketing & design building AB tests for
+                our Conversion team. Alongside another engineer and partnership of{' '}
+                <a styleName="link" href="https://www.carbonfive.com/">
+                    Carbon 5
+                </a>
+                , we built a Node.js static site using Jekyll. Later, my team grew and began focusing on a customized
+                static react app/rails server. During this time I led and broke down both internal and external facing
+                projects using&nbsp;Jira.
+            </p>
         </div>
-        <div styleName="full-width">
-            <div styleName="ft" href="https://www.flashtalking.com/">
-                <h2 styleName="h2">Flashtalking</h2>
-                <p styleName="p">
-                    Starting with ActionScript 2, I designed and built advertisement creatives for large and small
-                    businesses including clients such as Hulu, Gap, and CBS. I worked with our internal developers in
-                    Leeds and New York directly, to help improve internal webapp tooling. After HTML began dominating
-                    the industry, I worked mostly with CSS and JavaScript frameworks such as&nbsp;Three.js.
-                </p>
-            </div>
+        <img styleName="sfix-logo" src="./images/sfix_logo.png" />
+        <div styleName="ft" href="https://www.flashtalking.com/">
+            <h2 styleName="workplace">
+                <a href="https://www.flashtaking.com/">Flashtalking</a>
+            </h2>
+            <p styleName="p">
+                Starting with ActionScript 2, I designed and built advertisement creatives for large and small
+                businesses including clients such as Hulu, Gap, and CBS. I worked with our internal developers in Leeds
+                and New York directly, to help improve internal webapp tooling. After HTML began dominating the
+                industry, I worked mostly with CSS and JavaScript frameworks such as&nbsp;
+                <a styleName="link" href="https://threejs.org/">
+                    Three.js
+                </a>
+                .
+            </p>
         </div>
+        <img styleName="ft-logo" src="./images/ft_logo.png" />
     </div>
 );
 export default WorkPage;
