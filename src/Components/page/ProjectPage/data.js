f76e2a47e5bec66fe407a0da206f10c183aa0b0d
logancool <coollogan88@gmail.com>
2020-07-26 11:47:39 -0700

    add grid and stack to project page

---
+++ src/Components/page/ProjectPage/data.js
@@ -0,0 +1,18 @@
+const data = {
+    stack: {
+        proficient: [
+            'JavaScript ECMAScript 5/6',
+            ' Ruby',
+            ' Git',
+            ' JQuery',
+            ' Wordpress',
+            ' Python',
+            ' Java',
+            ' C++',
+            'zsh',
+        ],
+        familiar: ['Go', 'AWS', 'Rails'],
+    },
+};
+
+export default data;
--- src/Components/page/ProjectPage/index.tsx
+++ src/Components/page/ProjectPage/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import PageContent from '../../layout/PageContent';
 import './styles.module.scss';
+import data from './data';
 
-const ProjectPage = () => <PageContent>projects and stack coming soon...</PageContent>;
+const disableProjectPage = false;
+
+const ProjectPage = () => {
+    return disableProjectPage ? (
+        <PageContent>projects and stack coming soon...</PageContent>
+    ) : (
+        <PageContent>
+            <div styleName="page">
+                <div styleName="project-a">project a</div>
+                <div styleName="project-b">project b</div>
+                <div styleName="stack">
+                    <h1>Stack</h1>
+                    <h2>Proficient</h2>
+                    {data.stack.proficient.map(language => (
+                        <div>{language}</div>
+                    ))}
+                    <h2>Familiar</h2>
+                    {data.stack.familiar.map(language => (
+                        <div>{language}</div>
+                    ))}
+                </div>
+            </div>
+        </PageContent>
+    );
+};
 
 export default ProjectPage;
