79210ea525895c1539e6b1646b4a7e6b93e21ec0
logancool <coollogan88@gmail.com>
2020-07-26 00:27:51 -0700

    fix 404

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
                     <Route name="work" exact path="/work" component={WorkPage} />
                     <Route name="resume" exact path="/resume" component={ResumePage} />
                     <Route name="projects" exact path="/projects" component={ProjectPage} />
+                    <Route component={PageNotFound} />
                 </Switch>
             </div>
             <Footer />
